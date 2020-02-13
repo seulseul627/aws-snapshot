26493a5b1e9fb52b8e0d0a47baeda08cfc26093a
yeseul <seulseul627@gmail.com>
2020-02-13 15:42:56 +0900

    add print tag

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,10 +5,25 @@ session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
 @click.command()
-def list_instances():
-    "List EC2 instances"    # --help 옵션을 위해 추가
-    for i in ec2.instances.all():
-        print(i)
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+    instances = []
+
+    if project: # default값으로 들어오는 경우 false임
+        print("tag project : " + project)
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = { t['Key']:t['Value'] for t in i.tags or [] }
+        print(', '.join((
+            i.instance_type,
+            tags.get('Project', '<no project>')
+        )))
 
 if __name__ == '__main__':
     list_instances()
