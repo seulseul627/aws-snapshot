4fa4246beafce1a44cb08f5d7c807f68be0e08f5
yeseul <seulseul627@gmail.com>
2020-02-13 17:28:15 +0900

    click.group add

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aws-snapshot
 Demo project to manage AWS instance snapshots
+
+# Configuring
+shotty uses the configuration file created by the AWS cli. e.g.
+
+`aws configure --profile shotty`
+
+# Running
+`pipenv run python shotty/shotty.py <command> <--project=PROJECT>`
+
+*command* is list, start, or stop
+*project* is optional
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,11 +4,7 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-@click.option('--project', default=None,
-    help="Only instances for project (tag Project:<name>)")
-def list_instances(project):
-    "List EC2 instances"
+def filter_instances(project):
     instances = []
 
     if project: # default값으로 들어오는 경우 false임
@@ -18,12 +14,40 @@ def list_instances(project):
     else:
         instances = ec2.instances.all()
 
+    return instances
+
+@click.group()
+def instances():
+    """Command for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+
+    instances = filter_instances(project)
     for i in instances:
         tags = { t['Key']:t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.instance_type,
-            tags.get('Project', '<no project>')
+            tags.get('Project', '<no project>'),
+            i.state['Name']
         )))
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None,
+    help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = filter_instances(project)
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
