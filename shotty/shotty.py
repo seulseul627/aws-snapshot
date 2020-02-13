b3e99f23d64042b181e86bed1238d777c0fb47a4
yeseul <seulseul627@gmail.com>
2020-02-13 14:33:38 +0900

    Update script to use the click module

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,14 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
+@click.command()
+def list_instances():
+    "List EC2 instances"    # --help 옵션을 위해 추가
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == '__main__':
+    list_instances()
